da23ecae68587debbb4eaecf7b2dac9b12379d76
Grace Nieboer <grace.nieboer@outlook.com>
2022-11-27 20:31:46 -0500

    commit tree 569f8d25d37633dda2475c5645f4d532d1c7bc20

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solutions in this file
+
+
+  const gifts = ["teddy bear", "drone", "doll"];
+  function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+}
+return gifts    
+  }
+
+  wrapGifts(gifts);
+
+
+  function writeCards(array, event) {
+let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+}
+    
+    return newArray
+  }
+
+// example writeCards(["grace", "noah", "sam"], "surprise")
+
+function countDown(number) {
+    let count = number;
+    while (count >= 0) {
+        console.log(count);
+        count--;
+    }
+    return count
+}
+countDown(10);
+
+  
